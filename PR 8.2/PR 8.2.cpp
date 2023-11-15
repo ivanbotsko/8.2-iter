6d592088359db5aa6de2ea6134b38abef2208fb3
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-15 23:07:27 +0200

    first master commit

---
+++ PR 8.2/PR 8.2.cpp
@@ -0,0 +1,37 @@
+﻿// Дано літерний рядок, який містить послідовність символів s[0], ... , s[n], ...
+// Вилучити з цієї послідовності групи символів, які знаходяться між дужками «(», «)».
+// Самі дужки теж мають бути вилучені.
+// Якщо всередині деякої пари дужок є пара інших дужок(вкладені дужки), то вилучаються всі символи, які знаходяться між самими зовнішніми дужками і самі ці дужки.
+
+#include <iostream>
+
+using namespace std;
+
+string deleteBetweenBrackets(const string& str) {
+    string result = "";
+    int openBracketsCount = 0;
+
+    for (char ch : str) {
+        if (ch == '(') {
+            openBracketsCount++;
+        }
+        else if (ch == ')' && openBracketsCount > 0) {
+            openBracketsCount--;
+        }
+        else if (openBracketsCount == 0) {
+            result += ch;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    string str = "This is a string with (several) parentheses, (including (nested) parentheses).";
+
+    cout << str << endl;
+    cout << deleteBetweenBrackets(str) << endl;
+
+    return 0;
+}
+
